ba3f27935219dd538c57beaf6f040f8c626cb661
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2023-01-14 12:34:30 +0100

    update project

--- app/src/main/java/com/whiteraven777/launcherreplacer/MainActivity.java
+++ app/src/main/java/com/whiteraven777/launcherreplacer/MainActivity.java
@@ -13,7 +13,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
--- app/src/main/java/com/whiteraven777/launcherreplacer/SettingsActivity.java
+++ app/src/main/java/com/whiteraven777/launcherreplacer/SettingsActivity.java
@@ -2,7 +2,7 @@ package com.whiteraven777.launcherreplacer;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
